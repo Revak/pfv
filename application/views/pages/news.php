7e1ac341cb025aa87f70307bd4492d53cf1d83a2
Revak <louis.maciejewski@gmail.com>
2020-10-12 00:32:40 +0200

    Updated news page (styling + content)

--- application/views/pages/news.php
+++ application/views/pages/news.php
@@ -1,20 +1,62 @@
 <div id="news_container">
     <h3>Les nouveautés de la dernière version</h3>
-    
-    <dl>
-        <dt>Ravalement de façade</dt>
-        <dd>Le site est maintenant plus réactif et facile à lire.</dd>
 
-        <dt>Ajout d'un historique</dt>
-        <dd>Vous pouvez maintenant consulter les cadeaux demandés par chacun les années précédentes (à partir de 2017) sur la page <a href="<?= site_url('page/history'); ?>">Historique</a>.</dd>
-
-        <dt>Gestion autonome du mot de passe</dt>
-        <dd>Une fonction "mot de passe oublié" a été ajoutée. Il est maintenant aussi possible de modifier soi-même son mot de passe dans la page <a href="<?= site_url('page/editAccount'); ?>">Mon compte</a>.</dd>
-
-        <dt>Ajout des suggestions</dt>
-        <dd>Vous pouvez maintenant proposer des suggestions de cadeaux que tout le monde pourra voir sauf le destinataire du cadeau. Pour utiliser cette fonction, cliquez sur le bouton "Ajouter une suggestion" du panneau de droite sur la page des <a href="<?= site_url('page/giftList'); ?>">listes.</a></dd>
-
-        <dt>Ajout des notifications</dt>
-        <dd>Vous recevrez maintenant un email d'alerte quand quelqu'un ajoute un nouveau cadeau à sa liste. Plus besoin de consulter le site tous les jours pour être au courant des nouveautés ! Pour désactiver les alertes, rendez vous dans la page <a href="<?= site_url('page/editAccount'); ?>">Mon compte</a> et décochez l'option, puis enregistrez.</dd>
-    </dl>
+    <table id="changelog">
+      <tr>
+        <td colspan="2" class="version_number">v4 (2020)</td>
+      </tr>
+      <tr>
+        <td>Ajout de droits "observateur"</td>
+        <td>La vue d'un utilisateur peut maintenant être limitée à une/plusieurs personnes. Et un utilisateur peut ne pas avoir de liste.</td>
+      </tr>
+      <tr>
+        <td>Simplification de la création de compte dans l'admin</td>
+        <td class="no_content">-</td>
+      </tr>
+      <tr>
+        <td>Correction de bugs sur l'envoi des notifications</td>
+        <td class="no_content">-</td>
+      </tr>
+      <tr>
+        <td colspan="2" class="version_number">v3 (2018)</td>
+      </tr>
+      <tr>
+        <td>Ravalement de façade</td>
+        <td>Le site est maintenant plus réactif et facile à lire.</td>
+      </tr>
+      <tr>
+        <td>Ajout des suggestions</td>
+        <td>Vous pouvez maintenant proposer des suggestions de cadeaux que tout le monde pourra voir sauf le destinataire du cadeau. Pour utiliser cette fonction, cliquez sur le bouton "Ajouter une suggestion" du panneau de droite sur la page des <a href="<?= site_url('page/giftList'); ?>">listes.</a></td>
+      </tr>
+      <tr>
+        <td>Ajout d'un historique</td>
+        <td>Vous pouvez maintenant consulter les cadeaux demandés par chacun les années précédentes (à partir de 2017) sur la page <a href="<?= site_url('page/history'); ?>">Historique</a>.</td>
+      </tr>
+      <tr>
+        <td>Gestion autonome du mot de passe</td>
+        <td>Une fonction "mot de passe oublié" a été ajoutée. Il est maintenant aussi possible de modifier soi-même son mot de passe dans la page <a href="<?= site_url('page/editAccount'); ?>">Mon compte</a>.</dd></td>
+      </tr>
+      <tr>
+        <td>Ajout des notifications</td>
+        <td>Vous recevrez maintenant un email d'alerte quand quelqu'un ajoute un nouveau cadeau à sa liste. Plus besoin de consulter le site tous les jours pour être au courant des nouveautés ! Pour désactiver les alertes, rendez vous dans la page <a href="<?= site_url('page/editAccount'); ?>">Mon compte</a> et décochez l'option, puis enregistrez.</td>
+      </tr>
+      <tr>
+        <td colspan="2" class="version_number">v2 (2017)</td>
+      </tr>
+      <tr>
+        <td>Améliorations graphiques et fonctionnelles</td>
+        <td>Le site est plus simple à utiliser et le design moins daté.</td>
+      </tr>
+      <tr>
+        <td>Mots de passe cryptés</td>
+        <td>Les mots de passe ne sont plus en clair dans la base de donnée.</td>
+      </tr>
+      <tr>
+        <td colspan="2" class="version_number">v1 (2016)</td>
+      </tr>
+      <tr>
+        <td>Fonctionnalités de base</td>
+        <td>Vous pouvez ajouter des idées de cadeaux à votre liste et les autres utilisateurs peuvent les réserver. Les réservations sont invisibles pour le propriétaire d'un liste.</td>
+      </tr>
+    </table>
 </div>
